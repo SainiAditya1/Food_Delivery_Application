936bd044a04e32837e381e7b59189baab944b9cf
Aditya Saini <adityasaini10012001@outlook.com>
2023-10-16 18:27:02 +0530

    create a middleware and a function to handle possible error
    Signed-off-by: Aditya Saini <adityasaini10012001@outlook.com>

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from '../models/User.model.js';
 import bcryptjs from 'bcryptjs';
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
 
     const { username, email, password} = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -11,9 +11,11 @@ export const signup = async (req, res) => {
     res.status(201).json('User created successfully');
         
     } catch (error) {
-        res.status(500).json(error.message);
+        next(error);
         
     }
     
 
 };
+
+
