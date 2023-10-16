0a9da9aa6fa643bcae7e06ac58f745ccbb052561
Aditya Saini <adityasaini10012001@outlook.com>
2023-10-16 09:59:18 +0530

    create header component
    Signed-off-by: Aditya Saini <adityasaini10012001@outlook.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,22 @@
-import { BrowserRouter , Routes, Route} from 'react-router-dom';
-import Home from './pages/Home';
-import SignUp from './pages/SignUp';
-import Signin from './pages/SignIn';
-import About from './pages/About';
-import Profile from './pages/Profile';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import Signin from "./pages/SignIn";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
-     <Routes>
-       <Route path="/" element={<Home/>} />
-       <Route path="/sign-in" element={<Signin />} />
-       <Route path="/sign-up" element={<SignUp />} />
-       <Route path="/about" element={<About/>} />
-      <Route path="/profile" element={<Profile />} />
-
-
-     </Routes>
-    
-    
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </BrowserRouter>
-  )
-  
+  );
 }
